dca5fe44f919dddf096ac036ee93767d0df4920f
lomik <r.lomonosov@gmail.com>
2024-10-19 00:07:37 +0300

    set codec

--- example/simple/init.sql
+++ example/simple/init.sql
@@ -10,8 +10,8 @@ ENGINE = Null();
 
 CREATE TABLE samples (
 	`id` String,
-	`timestamp` Int64,
-	`value` Float64,
+	`timestamp` Int64 CODEC(T64),
+	`value` Float64 CODEC(Gorilla),
 )
 ENGINE = ReplacingMergeTree()
 ORDER BY (id, timestamp)
@@ -31,7 +31,7 @@ CREATE TABLE series (
 	`timestamp_max` SimpleAggregateFunction(max, Int64) DEFAULT `timestamp`
 )
 ENGINE = AggregatingMergeTree()
-ORDER BY (name, id, labels)
+ORDER BY (name, id)
 PARTITION BY intDiv(timestamp_min,86400000)*86400000 -- 1 day in ms
 SETTINGS min_age_to_force_merge_seconds = 3600, min_age_to_force_merge_on_partition_only = 1;
 
